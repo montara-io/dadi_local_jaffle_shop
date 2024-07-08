9ba989ab473870f4316839c3649def903d7074e3
Montara <montara@montara.io>
2024-07-08 08:25:46 +0000

    Update model test_data_retention

--- models/montara/models/test_data_retention.sql
+++ models/montara/models/test_data_retention.sql
@@ -4,13 +4,11 @@
 WITH raw_hosts_augmented AS (SELECT * FROM {{ ref('raw_hosts_augmented') }})
 
 select
-*
-from
+* from
 raw_hosts_augmented
 --</DBT_CODE>
 --<ORIGINAL_CODE>
 --select
---  *
---from
+--  * from
 --  raw_hosts_augmented
 --</ORIGINAL_CODE>
