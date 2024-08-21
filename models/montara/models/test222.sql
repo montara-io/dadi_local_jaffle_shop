dbb4d84c7fce7e7ae25eff0e62111172824fb3d1
Montara <montara@montara.io>
2024-08-21 17:33:28 +0000

    Update model test222

--- models/montara/models/test222.sql
+++ models/montara/models/test222.sql
@@ -3,10 +3,9 @@
 
 WITH cleansed_hosts AS (SELECT * FROM {{ ref('cleansed_hosts') }})
 
-select *
-from cleansed_hosts
+select * from cleansed_hosts
 --</DBT_CODE>
 --<ORIGINAL_CODE>
---select * 
---from cleansed_hosts
+---- new 
+--select * from cleansed_hosts
 --</ORIGINAL_CODE>
