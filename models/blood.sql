bc7c480c391ea213393937b87a5f4d8d050fb8a9
Montara <montara@montara.io>
2024-09-05 18:16:18 +0000

    Update model blood

--- models/blood.sql
+++ models/blood.sql
@@ -4,11 +4,9 @@
 WITH cleansed_hosts AS (SELECT * FROM {{ ref('cleansed_hosts') }})
 
 select
-*
-from cleansed_hosts
+* from cleansed_hosts
 --</DBT_CODE>
 --<ORIGINAL_CODE>
 --select
---  *
---from cleansed_hosts
+--  * from cleansed_hosts
 --</ORIGINAL_CODE>
