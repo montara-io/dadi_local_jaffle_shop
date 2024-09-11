42e266115067713d69ab55e579fa76532c30751a
Montara <montara@montara.io>
2024-09-11 08:01:12 +0000

    Update model test_pyt

--- models/staging/test_pyt.py
+++ models/staging/test_pyt.py
@@ -6,10 +6,9 @@ List sources and models in the function definition as below
 def test_pyt(sourceA, modelA)  
 '''
 def test_pyt(test2):
-  return model
+  return test2
 #</ORIGINAL_CODE>
 def model(dbt, session):
     test2 = dbt.ref('test2')
     result = test_pyt(test2)
-    
     return result
