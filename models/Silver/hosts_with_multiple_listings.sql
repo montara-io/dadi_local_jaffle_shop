0490a8b36b7a330ea7a58c5fbf5a7cb1c933161c
Montara <montara@montara.io>
2024-09-11 07:58:19 +0000

    Update model hosts_with_multiple_listings

--- models/Silver/hosts_with_multiple_listings.sql
+++ models/Silver/hosts_with_multiple_listings.sql
@@ -3,22 +3,14 @@
 
 WITH RAW_LISTINGS__RAW_LISTINGS AS ( SELECT * FROM {{ source('RAW_LISTINGS', 'RAW_LISTINGS') }})
 
-SELECT * FROM ( 
 select
 id as "actual list price: list price*quantity"
 from
 raw_listings__raw_listings
-) AS montara_model
---<INCREMENTAL>
-{% if is_incremental() %}
-WHERE updated_at >= (SELECT max(updated_at) FROM {{ this }})
-{% endif %}
---</INCREMENTAL>
 --</DBT_CODE>
 --<ORIGINAL_CODE>
 --select
 --  id as "actual list price: list price*quantity"
 --from
---  raw_listings__raw_listings 
---
+--  raw_listings__raw_listings
 --</ORIGINAL_CODE>
