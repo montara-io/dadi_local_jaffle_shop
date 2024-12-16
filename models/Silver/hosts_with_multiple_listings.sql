325075ecea5a9ae41d50e52a205a1784ca8d90ba
Montara <montara@montara.io>
2024-12-16 08:28:31 +0000

    Update model hosts_with_multiple_listings

--- models/Silver/hosts_with_multiple_listings.sql
+++ models/Silver/hosts_with_multiple_listings.sql
@@ -7,12 +7,12 @@ select
 *
 from
 cleansed_listings
-limit 10000
+limit 100
 --</DBT_CODE>
 --<ORIGINAL_CODE>
 --select
 --  *
 --from
 --  cleansed_listings
---  limit 10000
+--  limit 100
 --</ORIGINAL_CODE>
