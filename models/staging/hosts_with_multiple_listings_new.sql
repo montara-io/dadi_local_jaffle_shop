2b13006412dfdb6a37facc3ea93fc97fc7d48c77
Montara <montara@montara.io>
2025-02-12 15:19:25 +0000

    [Montara]: Update model hosts_with_multiple_listings_new

--- models/staging/hosts_with_multiple_listings_new.sql
+++ models/staging/hosts_with_multiple_listings_new.sql
@@ -8,7 +8,7 @@ select
 from
 cleansed_listings
 limit
-10000
+100
 --</DBT_CODE>
 --<ORIGINAL_CODE>
 --select
@@ -16,5 +16,5 @@ limit
 --from
 --  cleansed_listings
 --limit
---  10000
+--  100
 --</ORIGINAL_CODE>
